0fe0c9143b27ee4166ba61d45e77b5f8fe76185e
Shahzod <bektemirovshahzodbek@gmail.com>
2021-02-18 00:33:40 +0500

    Initial Commit

---
+++ secondGitAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  secondGitAPP
+//
+//  Created by shahzod on 2/18/21.
+//  Copyright © 2021 shahzod. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
